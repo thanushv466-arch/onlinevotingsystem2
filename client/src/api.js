ede25ec0328f9c44935535778169afe2459f529e
Thanush V <thanushv466@gmail.com>
2025-11-07 14:26:51 +0530

    updated api base url

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL:'https://digital-elections.onrender.com'});
+const API = axios.create({ baseURL:'https://digital-elections.onrender.com/api'});
 
 export default API;
 
