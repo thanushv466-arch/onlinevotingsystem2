71af6f305bb1aa530ab6b933931ac2c32ffd2ae8
Thanush V <thanushv466@gmail.com>
2025-11-07 13:58:54 +0530

    frontend updated

--- client/src/api.js
+++ client/src/api.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL:'https://digital-elections.onrender.com'});
+   
 
+
+
+                                
 export default API;
+
